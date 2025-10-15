4abccb5875322ce5607b88e4a631acc9a7dd058e
Nguyễn Hoàng Thiên <23010139@st.phenikaa-uni.edu.vn>
2025-10-15 14:33:19 +0000

    html & css

--- src/main/java/com/autosys/service/HeThongQuanLy.java
+++ src/main/java/com/autosys/service/HeThongQuanLy.java
@@ -17,25 +17,25 @@ public class HeThongQuanLy {
     public HeThongQuanLy() {
         // Seed enterprise hierarchy
         center = new CuaHangChinh("Honda Vietnam", "Việt Nam", "Miền Bắc", "Honda Center Hà Nội", "Hà Nội");
-        center.themXe(new Car("A001", "Civic 2024", "Honda", MauSac.TRANG, 850_000_000, "civic2024.svg"));
-        center.themXe(new Car("A002", "CR-V 2024", "Honda", MauSac.DEN, 1_100_000_000, "crv2024.svg"));
+        center.themXe(new Car("A001", "Civic 2024", "Honda", MauSac.TRANG, 850_000_000, "hondacivic2024.jpg"));
+        center.themXe(new Car("A002", "CR-V 2024", "Honda", MauSac.DEN, 1_100_000_000, "hodacrv2024.jpg"));
 
         PhongBan sales = new PhongBan("Honda Vietnam", "Việt Nam", "Miền Bắc", "Honda Center Hà Nội", "Hà Nội", "Bán Hàng");
-        sales.getDanhSachXe().add(new Car("A003", "City 2024", "Honda", MauSac.BAC, 560_000_000, "city2024.svg"));
+        sales.getDanhSachXe().add(new Car("A003", "City 2024", "Honda", MauSac.BAC, 560_000_000, "hondacity2024.jpg"));
         npp.add(sales);
 
         CuaHangUyNhiem hcm = new CuaHangUyNhiem("Honda Vietnam", "Việt Nam", "Miền Nam", "Honda Quận 7", "TP. HCM");
-        hcm.themXe(new Car("B101", "HR-V 2024", "Honda", MauSac.DO, 899_000_000, "hrv2024.svg"));
+        hcm.themXe(new Car("B101", "HR-V 2024", "Honda", MauSac.DO, 899_000_000, "hodahrv2024.jpg"));
         authorized.add(hcm);
         npp.add(hcm);
 
         ChiNhanhDiaPhuong hcmA = new ChiNhanhDiaPhuong("Honda Vietnam", "Việt Nam", "Miền Nam", "Honda Quận 7 - Cơ sở A", "Quận 7", "Tân Phong");
-        hcmA.getDanhSachXe().add(new Car("B102", "City 2024", "Honda", MauSac.XANH, 560_000_000, "city2024.svg"));
+        hcmA.getDanhSachXe().add(new Car("B102", "City 2024", "Honda", MauSac.XANH, 560_000_000, "hondacity2024.jpg"));
         branches.add(hcmA);
         npp.add(hcmA);
 
         CuaHangUyNhiem dn = new CuaHangUyNhiem("Honda Vietnam", "Việt Nam", "Miền Trung", "Honda Đà Nẵng", "Đà Nẵng");
-        dn.themXe(new Car("C201", "CR-V 2024", "Honda", MauSac.TRANG, 1_100_000_000, "crv2024.svg"));
+        dn.themXe(new Car("C201", "CR-V 2024", "Honda", MauSac.TRANG, 1_100_000_000, "hodacrvt2024.jpg"));
         authorized.add(dn);
         npp.add(dn);
     }
